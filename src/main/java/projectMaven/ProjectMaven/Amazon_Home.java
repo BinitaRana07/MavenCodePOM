1fe9282e0bf6e61a6fa1d72a582cd0dbbbb8df23
BinitaRana07 <107548478+BinitaRana07@users.noreply.github.com>
2025-10-23 14:41:46 +0100

    Amazon_Home.java

--- src/main/java/projectMaven/ProjectMaven/Amazon_Home.java
+++ src/main/java/projectMaven/ProjectMaven/Amazon_Home.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.asserts.SoftAssert;
+//import org.testng.asserts.SoftAssert;
 
 public class Amazon_Home 
 {
